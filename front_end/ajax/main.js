ccacc41320083e8d119e38bd14d4c237371229a7
--global <leoking9641@hotmail.com>
2018-07-13 23:44:02 +0800

    add onprogress handle

--- front_end/ajax/main.js
+++ front_end/ajax/main.js
@@ -1,21 +1,35 @@
-var apiUri = 'https://api.github.com/users/tatwd';
+var loading = document.querySelector('#loading');
+var apiUri = 'https://api.github.com/users';
 
 var xhr = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
 
 xhr.onreadystatechange = function() {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
-      console.log(xhr);
+      // console.log(xhr);
       document.querySelector('#users').innerHTML = xhr.responseText;
     }
   }
 };
 
-/*
 xhr.onprogress = function(evt) {
-  console.log(evt);
+  // console.log(evt.lengthComputable);
+  if (evt.lengthComputable) {
+    loading.max = evt.total;
+    loading.value = (evt.loaded / evt.total) * 100;
+  }
 };
-*/
+
+xhr.onloadend = function(evt) {
+  loading.value = evt.loaded;
+};
 
 xhr.open('GET', apiUri, true);
 xhr.send(null);
+
+// async is false
+/*
+xhr.open('GET', apiUri + '/2', false);
+xhr.send(null);
+document.querySelector('#users').innerHTML = xhr.responseText;
+*/
