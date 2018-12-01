1e0b2342a63853ae69ed1f67b485bb5510b3b48f
--global <leoking9641@hotmail.com>
2018-12-02 00:23:40 +0800

    sorting: change  to improv pref

--- sorting/selection_sort/main.c
+++ sorting/selection_sort/main.c
@@ -7,7 +7,7 @@ void selection_sort(int arr[], int n) {
   int i, j, min;
   for (i = 0; i < n - 1; i++) {
     min = i;
-    for (j = i; j < n; j++) {
+    for (j = i + 1; j < n; j++) {
         if (arr[j] < arr[min])
             min = j;
     }
