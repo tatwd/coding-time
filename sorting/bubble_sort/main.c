4cd461801ec0ba4e627e48681fb3a8ff70c039f7
--global <leoking9641@hotmail.com>
2019-03-30 17:20:38 +0800

    sorting(bubble): update and add recursive stuff

--- sorting/bubble_sort/main.c
+++ sorting/bubble_sort/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/*
-extern void swap(int *a, int *b);
-*/
+/* extern void swap(int *a, int *b); */
 #include "utils.h"
 
 /**
  * 冒泡排序
  */
 void bubble_sort(int arr[], int n) {
-    int i, j, sorted;
+    /*
+	int i, j, sorted;
     for (i = 0; i < n -1; i++) {
         sorted = 0;
         for (j = 0; j < n - i -1; j++) {
@@ -19,8 +18,35 @@ void bubble_sort(int arr[], int n) {
             }
         }
         if (!sorted)
-            break; /* now array is sorted */
+            break;
     }
+	*/
+	/*
+	int i, j, sorted;
+	for (i = n - 1; i > 0; i--) {
+		sorted = 0;
+		for (j = 0; j < i; j++) {
+		if (arr[j] > arr[j + 1]) {
+			swap(&arr[j], &arr[j+1]);
+			sorted = 1;
+		}
+		}
+		if (!sorted)
+			break;
+	}
+	*/
+
+	/* 递归版 */
+	int i, sorted = 0;
+	for (i = 0; i < n - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			swap(&arr[i], &arr[i + 1]);
+			sorted = 1;
+		}
+	}
+	if (!sorted)
+		return;
+	bubble_sort(arr, n - 1);
 }
 
 int main(void)
