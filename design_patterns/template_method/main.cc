9a9ff65f020c2c065de27481268e048056305ade
--global <leoking9641@hotmail.com>
2019-04-10 10:42:14 +0800

    dp: impl template pattern

---
+++ design_patterns/template_method/main.cc
@@ -0,0 +1,29 @@
+/* 模板代码 p.214 */
+
+#include <iostream>
+
+class Operator
+{
+public:
+	int Add(int a, int b) {
+		return First(a) + Last(b);
+	}
+	virtual int First(int v) { return v; }
+	virtual int Last(int v) { return v; }
+};
+
+class OperatorImpl : public Operator
+{
+public:
+	virtual int First(int v) { return ++v; }
+	/* virtual int Last(int v) { return ++v; } */
+};
+
+int main(void)
+{
+	Operator *opt = new OperatorImpl;
+	int result = opt->Add(1, 2);
+	std::cout << result << std::endl;
+	delete opt;
+	return 0;
+}
