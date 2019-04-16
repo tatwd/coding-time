f7e7c4a9f60d4d3572cd3a8566120484fdf71833
--global <leoking9641@hotmail.com>
2019-04-17 01:02:13 +0800

    sorting(merge): fix and test it ok

--- sorting/merge_sort/main.c
+++ sorting/merge_sort/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 /* 合并两段有序的数组 */
+/*
 void merge(int arr[], int n)
 {
 	int *tmp = malloc(n * sizeof(int));
@@ -21,17 +22,61 @@ void merge(int arr[], int n)
 		arr[i] = tmp[i];
 	free(tmp);
 }
+*/
+void merge(int arr[], int tmp[],
+           int left, int mid, int right)
+{
+	/* `mid` belongs to right part. */
+
+	int rt_start, i, n;
+	rt_start = mid; /* right part start index */
+	i = left; /* tmp array start index */
+	n = right - left + 1; /* number of elements */
+
+	while(left < mid && rt_start <= right) {
+		tmp[i++] = arr[left] <= arr[rt_start]
+			? arr[left++]
+			: arr[rt_start++];
+	}
+	while(left < mid) tmp[i++] = arr[left++];
+	while(rt_start <= right) tmp[i++] = arr[rt_start++];
+
+	/* copy to arr */
+	for (i = 0; i < n; i++)
+		arr[i] = tmp[i];
+}
+
+void msort(int arr[], int tmp[],
+           int left, int right)
+{
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		msort(arr, tmp, left, mid);
+		msort(arr, tmp, mid + 1, right);
+		merge(arr, tmp, left, mid, right);
+	}
+}
 
 void merge_sort(int arr[], int n)
 {
-	/* TODO */
+
+	int *tmp = malloc(n * sizeof(int));
+	if (tmp != NULL) {
+		msort(arr, tmp, 0, n - 1);
+		free(tmp);
+	} else {
+		printf("No space for tmp\n");
+	}
 }
 
 /* 测试 */
 int main(void)
 {
-	int arr[] = { 1, 2, 5, 7, 3, 4, 6 }; /* 两段已有序 */
-	merge(arr, 7);
+	// int arr[] = { 1, 2, 5, 7, 3, 4, 6 }; /* 两段已有序 */
+	int arr[] = { 3, 1, 4, 2, 5, 7, 6 };
+	// merge(arr, 7);
+	merge_sort(arr, 7);
 	for (int i = 0; i < 7; i++)
 		printf("%d ", arr[i]);
 	return 0;
