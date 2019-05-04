05edeb4063975850ddc995386d5c34eb3a3adb0c
tatwd <leoking9641@hotmail.com>
2019-05-04 22:39:35 +0800

    coding(math): add recursive impl for gcd

--- coding/math/gcd.c
+++ coding/math/gcd.c
@@ -3,6 +3,8 @@ extern void swap(int *a, int *b);
 /**
  * 求最大共因数（GCD）
  * 运行时间 O(log N)
+ *
+ * 欧几里得算法
  */
 unsigned int
 gcd(unsigned int m, unsigned int n) {
@@ -16,4 +18,11 @@ gcd(unsigned int m, unsigned int n) {
         n = rem;
     }
     return m;
+}
+
+/* 递归实现 */
+unsigned int
+gcd2(unsigned int m, unsigned int n) {
+    if (m < n) swap(&m, &n);
+    return n == 0 ? m : gcd2(n, m % n);
 }
